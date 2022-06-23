79001288772b53f20eb76f1193c10de954e889b7
dewslyse <dewslyse@gmail.com>
2022-06-23 19:23:53 +0000

    Add tests for fizzbuzz

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -14,5 +14,17 @@ describe Solver do
     it 'Reverses string' do 
       expect(@solver.reverse('Hello')).to eq 'olleH'
     end
+
+    it 'checks for fizzbuzz' do
+      expect(@solver.fizzbuzz(15)).to eq 'fizzbuzz'
+    end
+
+    it 'checks for buzz' do
+      expect(@solver.fizzbuzz(5)).to eq 'buzz'
+    end
+
+    it 'checks for fizz' do
+      expect(@solver.fizzbuzz(3)).to eq 'fizz'
+    end
   end
 end
