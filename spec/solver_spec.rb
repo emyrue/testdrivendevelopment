912a1c04d2c2fc5a18fca734bd9d82388e4ca256
dewslyse <dewslyse@gmail.com>
2022-06-23 19:19:49 +0000

    Add test for string reversal

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -4,12 +4,15 @@ describe Solver do
   context 'Testing solver methods' do
     before :each do
       @solver = Solver.new
-      allow(@solver).to receive(:reverse)
       allow(@solver).to receive(:fizzbuzz)
     end
 
     it 'Factorial returns correct answer' do
       expect(@solver.factorial(4)).to eq 24
+    end
+
+    it 'Reverses string' do 
+      expect(@solver.reverse('Hello')).to eq 'olleH'
     end
   end
 end
